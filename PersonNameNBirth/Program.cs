82afe09a7b7658dd3f8e21d42972b3276d4a87d7
ohtomu <150762200+ohtomu@users.noreply.github.com>
2024-08-18 13:40:50 -0300

    files modified

---
+++ PersonNameNBirth/Program.cs
@@ -0,0 +1,12 @@
+﻿using TestProject.Components;
+
+//16, "cleber glauco", "11/01/2000"
+Person cleber = new Person("cleber rocha", "1998/12/23");
+Person felipe = new Person("felipe glauco", "2005/08/19");
+
+cleber.name = "cleber glauco";
+cleber.Print();
+
+felipe.name = "felipe rocha";
+
+felipe.Print();
