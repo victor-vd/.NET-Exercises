b3a7374cc0562ccc7ae68b881dddac7d28275841
ohtomu <150762200+ohtomu@users.noreply.github.com>
2024-08-21 20:00:18 -0300

    add more information on the requirement

---
+++ ClientLogin/ClientLogin.Common/Models/Person.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientLogin.Components
+{
+    public class Client
+    {
+
+        DateTime now = DateTime.Now;
+        public int[] currentDate = new int[3];
+        private string name { get; set; }
+        private uint age { get; set; }
+        private string birthday { get; set; }
+        private string email { get; set; }
+        private string password { get; set; }
+        private int[] birthdayArr = new int[3];
+
+        public Client()
+        {
+            currentDate = [now.Year, now.Month, now.Day];
+            name = "";
+            age = 0;
+            birthday = "";
+            email = "";
+            password = "";
+        }
+        public void Check()
+        {
+            Console.WriteLine($"\nName: {name}\nAge: {age}\nBirthday: {birthday}\nEmail: {email}\nPassword: {password}\n");
+        }
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value.Length < 100)
+                {
+                    name = value;
+                }
+            }
+        }
+        public uint Age
+        {
+            get { return age; }
+        }
+        public string Birthday
+        {
+            get { return birthday; }
+            set
+            {
+                birthday = value;
+                birthdayArr = Array.ConvertAll(birthday.Split("/"), int.Parse);
+                age = (uint)(now.Year - birthdayArr[0]);
+                if (now.Year >= birthdayArr[0] && now.Month >= birthdayArr[1] && now.Day >= birthdayArr[2])
+                {
+                    age++;
+                }
+            }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                if (value.Length < 40)
+                {
+                    password = value;
+                }
+            }
+        }
+    }
+}
---
+++ ClientLogin/ClientLogin/Program.cs
@@ -0,0 +1,32 @@
+﻿using ClientLogin.Components;
+
+//16, "cleber glauco", "11/01/2000"
+Client cleber = new Client();
+Client felipe = new Client();
+
+
+cleber.Name = "cleber rocha";
+cleber.Birthday = "1998/12/23";
+cleber.Email = "cleber1123123213111@gmail.com";
+cleber.Password = "QAeBaefUE*¨&fV*)EÄbv)*VB";
+
+cleber.Check();
+
+cleber.Name = "cleber glauco";
+cleber.Birthday = "2005/08/19";
+
+cleber.Check();
+
+
+felipe.Name = "felipe rocha";
+felipe.Birthday = "1978/11/13";
+felipe.Email = "felipe1123123111@gmail.com";
+felipe.Password = "AYV&GHF*)Evb7¨*(&R)";
+
+felipe.Check();
+
+felipe.Name = "felipe glauco";
+felipe.Birthday = "2002/04/14";
+
+felipe.Check();
+
